46aad852d3b9f012d7b79d7d79f5f5013c3432a5
unknown <claralizarralde.s@gmail.com>
2024-04-15 19:43:39 -0300

    15/4 avance un poquittitititito

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,11 @@ export default function Home() {
     <>
       <Head>
         <title>POKEDEX 2.0</title>
-       </Head>
+        <link rel="shortcut icon" href="./images/pokeball6.png"></link>
+      </Head>
       <main className="main">
         <Headerbar></Headerbar>
+        <PokemonList></PokemonList>
       </main>
     </>
   );
@@ -43,7 +45,8 @@ export const Headerbar = () => {
   < div className="headerbar container-fluid">
     <div className="row">
       <div className="col-12 col-md-7 box">
-        <div className="logo"><Image className="imagelogo" width={150} height={80} src='/images/pokemonlogo.png' alt="pokemon logo"/></div>
+        <div className="logo">
+          <Image className="image-logo" width={150} height={80} src='/images/pokemonlogo.png' alt="pokemon logo"/></div>
         </div>
     <div className="col-6 col-md-3 box">
       <div className="buscador"><input placeholder="buscar" /></div>
@@ -67,3 +70,150 @@ export const Headerbar = () => {
     
  
 }
+
+export const PokemonList = () => {
+
+  const   [pokemonData, setPokemonData] = React.useState ([])
+  const   [pokemonInfo, setPokemonInfo] = React.useState (null)
+ 
+
+
+     const grabInfo = () => {
+     getPokemons().then(setPokemonData);
+
+     }
+
+     React.useEffect(() => {
+      grabInfo();
+      console.log(pokemonData);
+      }, [pokemonData]);
+
+    const grabPokemonInfo = (url) => {
+      getPokemonInfo(url).then(data => {
+        setPokemonInfo(data);
+      });
+    };
+  
+    React.useEffect(() => {
+      if (pokemonData.length > 0) {
+        grabPokemonInfo(pokemonData[0].url);
+      }
+    }, [pokemonData]);
+
+  
+
+  return (
+      <>
+      <div className="tarjetas-pokemon">
+  <div className="tarjeta">
+      <div className="id-back">#025</div> 
+      <div className="pokemon-img">  
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu"></img></div>
+      <div className="pokemon-info">
+            <div className="nombre-contenedor">
+                <p className="pokemon-id">#025</p>
+                <h2 className="pokemon-nombre">Pikachu</h2>
+          </div>
+       <div className="pokemon-tipos">
+                <p className="electric tipo">ELECTRIC</p>
+                <p className="fighting tipo">FIGHTING</p>
+            </div>
+       <div className="pokemon-stats">
+                <p className="stat">4m</p>
+                <p className="stat">60kg</p>
+            </div>
+      </div>
+ </div>
+ <div className="tarjeta">
+      <div className="id-back">#025</div> 
+      <div className="pokemon-img">  
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu"></img></div>
+      <div className="pokemon-info">
+            <div className="nombre-contenedor">
+                <p className="pokemon-id">#025</p>
+                <h2 className="pokemon-nombre">Pikachu</h2>
+          </div>
+       <div className="pokemon-tipos">
+                <p className="electric tipo">ELECTRIC</p>
+                <p className="fighting tipo">FIGHTING</p>
+            </div>
+       <div className="pokemon-stats">
+                <p className="stat">4m</p>
+                <p className="stat">60kg</p>
+            </div>
+      </div>
+ </div>
+ <div className="tarjeta">
+      <div className="id-back">#025</div> 
+      <div className="pokemon-img">  
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu"></img></div>
+      <div className="pokemon-info">
+            <div className="nombre-contenedor">
+                <p className="pokemon-id">#025</p>
+                <h2 className="pokemon-nombre">Pikachu</h2>
+          </div>
+       <div className="pokemon-tipos">
+                <p className="electric tipo">ELECTRIC</p>
+                <p className="fighting tipo">FIGHTING</p>
+            </div>
+       <div className="pokemon-stats">
+                <p className="stat">4m</p>
+                <p className="stat">60kg</p>
+            </div>
+      </div>
+ </div>
+ <div className="tarjeta">
+      <div className="id-back">#025</div> 
+      <div className="pokemon-img">  
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu"></img></div>
+      <div className="pokemon-info">
+            <div className="nombre-contenedor">
+                <p className="pokemon-id">#025</p>
+                <h2 className="pokemon-nombre">Pikachu</h2>
+          </div>
+       <div className="pokemon-tipos">
+                <p className="electric tipo">ELECTRIC</p>
+                <p className="fighting tipo">FIGHTING</p>
+            </div>
+       <div className="pokemon-stats">
+                <p className="stat">4m</p>
+                <p className="stat">60kg</p>
+            </div>
+      </div>
+ </div>
+
+ <div className="tarjeta">
+      <div className="id-back">#025</div> 
+      <div className="pokemon-img">  
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu"></img></div>
+      <div className="pokemon-info">
+            <div className="nombre-contenedor">
+                <p className="pokemon-id">#025</p>
+                <h2 className="pokemon-nombre">Pikachu</h2>
+          </div>
+       <div className="pokemon-tipos">
+                <p className="electric tipo">ELECTRIC</p>
+                <p className="fighting tipo">FIGHTING</p>
+            </div>
+       <div className="pokemon-stats">
+                <p className="stat">4m</p>
+                <p className="stat">60kg</p>
+            </div>
+      </div>
+ </div>
+  </div> </>
+  )
+
+}
+
+export const getPokemons = () => {
+         let limit = 10000
+         let offset = 0
+         let page = 1
+         const str = '?limit=' + limit + '&offset=' + offset + '&page=' + page
+          return new Promise ((resolve, reject) => {
+          fetch ("https://pokeapi.co/api/v2/pokemon" + str).then((response) => {
+          return response.json();
+          } ) .then(resolve).catch(reject)
+    }
+  )}
